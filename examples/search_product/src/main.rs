7b8fe1ff103200293868f4d013b5afe0d3b3bfdc
Vishal Das <26341736+dvishal485@users.noreply.github.com>
2025-05-19 14:35:46 +0530

    update examples

--- examples/search_product/src/main.rs
+++ examples/search_product/src/main.rs
@@ -1,14 +1,25 @@
 use flipkart_scraper::ProductSearch;
+use flipkart_scraper::search::{SortOrder, SearchParams};
 use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let query = "samsung washing machine";
-    let details = ProductSearch::search(query.into()).await;
+    let params = SearchParams {
+        page_number: Some(1),
+        sort: Some(SortOrder::PriceLowToHigh),
+        min_price: Some(10000),
+        max_price: Some(20000),
+        others: None,
+    };
+
+    let details = ProductSearch::search(query.into(), params).await;
+
     if let Ok(s) = details {
         println!("{:#?}\n\nTotal {} search results.", s, s.results.len());
     } else {
         println!("{}", details.unwrap_err());
     }
+    
     Ok(())
 }
